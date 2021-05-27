f39207b228cb6bd86327d3cf871cb6a73d28569d
Tiago Soares <fullcodebr@gmail.com>
2021-05-27 09:08:10 -0300

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8656b833724ffa8f58e713bfa4a57dba'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fdec60b9-1236-4938-bc89-104027893353'
+};
